d519e823d9c9bd4e4f550721b6182fca7702ff47
o-shun <zyaooo99@yahoo.co.jp>
2016-07-04 17:14:45 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -8,9 +8,9 @@ for(var i = 1; i < 101 ; i++)
 {
  var li = document.createElement('li');
  if(0 == i % 3)
-  li.textContent = Fizz ; 
+  li.textContent = 'Fizz' ; 
  if(0 == i % 5)
-  li.textContent = Buzz ;
+  li.textContent = 'Buzz' ;
  if(0 != i % 3 && 0 != i % 5)
  li.textContent = i; 
 document.getElementById('list').appendChild(li);
